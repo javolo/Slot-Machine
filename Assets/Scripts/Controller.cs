e031baa0680573100df826453dc1a94f23141963
mirceacatana <mircea.catalin.catana@gmail.com>
2015-12-11 17:36:34 +0200

    Project Setup & Basic Functionality

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+    public Spin spinner1;
+    public Spin spinner2;
+    public Spin spinner3;
+
+    public void CheckItems() {
+        int i1 = spinner1.GetItem();
+        int i2 = spinner2.GetItem();
+        int i3 = spinner3.GetItem();
+
+        if (i1 == i2 && i2 == i3) {
+            Debug.Log("3 of a kind!");
+        } else if (i1 == i2 || i1 == i3 || i2 == i3) {
+            Debug.Log("2 of a kind!");
+        }
+    }
+}
---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour {
+
+    public Controller gameController;
+
+    const int nrItems = 18;
+    bool isSpinning = false;
+    int frameCounter = 0;
+    int spinFrames = 0;
+
+    int item = 0;
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            spinFrames = Random.Range(90, 130);
+            item = (item + spinFrames) % (nrItems / 2);
+            frameCounter = 0;
+            isSpinning = true;
+        }
+
+        if (isSpinning) {
+            transform.Rotate(new Vector3(0.0f, 1.0f, 0.0f), -360.0f / nrItems);
+
+            if (frameCounter < spinFrames) {
+                ++frameCounter;
+            } else {
+                isSpinning = false;
+                gameController.CheckItems();
+            }
+        }
+	}
+
+    public int GetItem() {
+        return item;
+    }
+}
